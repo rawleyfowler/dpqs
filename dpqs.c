594a64dad3f6bd92a746eb3bc8955028829d465c
Rawley Fowler <rawleyfowler@gmail.com>
2023-03-22 22:58:50 -0500

    checkout princeton dpqs

--- dpqs.c
+++ dpqs.c
@@ -1,22 +1,26 @@
 /**
   Permission is hereby granted, free of charge,
-  to any person obtaining a copy of this software and associated documentation files (the “Software”),
-  to deal in the Software without restriction,
-  including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
-  and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
-  subject to the following conditions:
+  to any person obtaining a copy of this software and associated documentation
+  files (the “Software”), to deal in the Software without restriction, including
+  without limitation the rights to use, copy, modify, merge, publish,
+  distribute, sublicense, and/or sell copies of the Software, and to permit
+  persons to whom the Software is furnished to do so, subject to the following
+  conditions:
 
-  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+  The above copyright notice and this permission notice shall be included in all
+  copies or substantial portions of the Software.
 
-  THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
-  INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
   IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
-  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
-  ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+  DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+  OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
+  OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
 #include "dpqs.h"
+#include <stdint.h>
 #include <sys/types.h>
 
 void swap(int64_t a[], u_int64_t i, u_int64_t j) {
@@ -29,14 +33,14 @@ void swap(int64_t a[], u_int64_t i, u_int64_t j) {
  * Using Hoare's partition scheme
  */
 u_int64_t dpqs_qs_partition(int64_t array[], u_int64_t lo, u_int64_t hi) {
-  int64_t p = array[(int64_t) (floor((hi - lo) / 2.0) + lo)];
+  int64_t p = array[(int64_t)((hi - lo) >> 1) + lo];
 
   u_int64_t i, j;
   for (i = lo, j = hi;; i++, j--) {
-    while(array[i] < p)
+    while (array[i] < p)
       i++;
-    
-    while(array[j] > p)
+
+    while (array[j] > p)
       j--;
 
     if (i >= j)
@@ -55,88 +59,55 @@ void dpqs_insertion_sort(int64_t array[], u_int64_t lo, u_int64_t hi) {
   for (u_int64_t i = lo, j = i; i < hi; j = i++) {
     int64_t e = array[i + 1];
     while (e < array[j]) {
-      array[j+1] = array[j];      
+      array[j + 1] = array[j];
       if (j-- == lo)
         break;
     }
-    array[j+1] = e;
+    array[j + 1] = e;
   }
 }
 
 /**
  * Double pivot quicksort
  */
-void dpqs_partition(int64_t array[], u_int64_t lo, u_int64_t hi, u_int64_t div) {
-  u_int64_t len = (hi + 1) - lo;
+void dpqs_partition(int64_t array[], u_int64_t lo, u_int64_t hi) {
+  if (hi <= lo)
+    return;
+  
+  u_int64_t len = hi - lo;
   
   if (len < INSERTION_THRESH) {
     dpqs_insertion_sort(array, lo, hi);
     return;
   }
- 
-  u_int64_t thr = len / div;
-  u_int64_t m1 = lo + thr, m2 = hi - thr;
 
-  if (m1 <= lo)
-    m1 = lo + 1;
-  if (m2 >= hi)
-    m2 = hi - 1;
+  if (array[lo] > array[hi])
+    swap(array, lo, hi);
 
-  if (array[m1] < array[m2]) {
-    swap(array, m1, lo);
-    swap(array, m2, hi);
-  } else {
-    swap(array, m1, hi);
-    swap(array, m2, lo);
+  int64_t p = array[lo], q = array[hi];
+
+  u_int64_t lt = lo + 1, gt = hi - 1;
+  u_int64_t l = lo + 1;
+  while (l <= gt) {
+    if (array[l] > array[lo])
+      swap(array, lt++, l++);
+    else if (array[hi] < array[l])
+      swap(array, l, gt--);
+    else
+      l++;
   }
 
-  int64_t p1 = array[lo], p2 = array[hi];
+  swap(array, lo, --lt);
+  swap(array, hi, ++gt);
 
-  u_int64_t less = lo + 1;
-  u_int64_t more = hi - 1;
-  for (u_int64_t i = lo + 1, j = hi - 1; i < more; i++) {
-    if (array[i] < p1) {
-      swap(array, i, less++);
-    } else if (array[i] > p2) {
-      while(i < more && array[more] > p2) {
-        more--;
-      }
-      
-      swap(array, i, more--);
-
-      if (array[i] < p1)
-        swap(array, i, less++);
-    }
-  }
-
-  u_int64_t dist = more - less;
-  if (dist < 13)
-    thr++;
-
-  swap(array, less - 1, lo);
-  swap(array, more + 1, hi);
-
-  if (dist > len - 13 && p1 != p2) {
-    for (u_int64_t i = less; i <= more; i++) {
-      if (array[i] == p1)
-        swap(array, i, less++);
-      else if (array[i] == p2) {
-        swap(array, i, more--);
-
-        if (array[i] == p2)
-          swap(array, i, less++);
-      }
-    }
-  }
-
-  if (p1 < p2)
-    dpqs_partition(array, less, more, div);
+  dpqs_partition(array, lo, lt - 1);
+  if (array[lt] < array[gt])
+    dpqs_partition(array, lt + 1, gt - 1);
+  dpqs_partition(array, gt + 1, hi);
 }
 
 void dpqs_sort(int64_t array[], u_int64_t lo, u_int64_t hi) {
-  if (lo >= hi ||
-      hi < 0   ||
-      lo < 0) {
+  if (lo >= hi || hi < 0 || lo < 0 || hi - lo < 2) {
     return;
   }
 
@@ -148,10 +119,10 @@ void dpqs_sort(int64_t array[], u_int64_t lo, u_int64_t hi) {
   } else if (elems < QUICKSORT_THRESH) {
     p = dpqs_qs_partition(array, lo, hi);
   } else {
-    dpqs_partition(array, lo, hi, 3);
+    dpqs_partition(array, lo, hi);
     return;
   }
-  
+
   dpqs_sort(array, lo, p);
   dpqs_sort(array, p + 1, hi);
 }
@@ -160,7 +131,7 @@ int main() {
   int64_t i[9999];
 
   for (size_t j = 0; j < 9999; j++) {
-    i[j] = (int64_t) rand() / 1000000;
+    i[j] = (int64_t)rand() / 1000000;
   }
 
   dpqs_sort(i, 0, 9998);
